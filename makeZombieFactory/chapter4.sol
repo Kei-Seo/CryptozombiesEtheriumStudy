01c91006139dadb634a4f99327e11ee9bdd7eb65
kei_Seo <67668805+Kei-Seo@users.noreply.github.com>
2022-04-02 14:37:54 +0900

    Update and rename chaper4.sol to chapter4.sol

--- makeZombieFactory/chapter4.sol
+++ makeZombieFactory/chapter4.sol
@@ -4,6 +4,12 @@ contract ZombieFactory {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
     // 여기서 시작
 
 }
